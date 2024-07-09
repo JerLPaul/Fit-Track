b0512b9b2129e7f68dabd506be0e8762d70faace
Jerrell Paul <jerrell@uoguelph.ca>
2024-07-09 19:19:09 -0400

    added temp logo and updated footer

--- react/layouts/navandfooter.js
+++ react/layouts/navandfooter.js
@@ -1,17 +1,20 @@
 import styles from "../styles/nav.module.css"
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function NavAndFooter({ children }) {
   return (
     <div>
         <Head>
             <title>Fit-Track</title>
-            <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" href="/logo.ico" />
         </Head>
 
         <main>
             <nav className={styles.navbar}>
                 <ul className={styles.navlist}>
+                    <li className={styles.navitem}> <a href="/"> <Image src="/logo.svg" alt="Fit-Track Logo" width={40} height={30}/> </a></li>
+                    <li className={styles.navitem}> <a href="/">Fit-Track</a></li>
                     <li className={styles.navitem}> <a href="/">Home</a></li>
                     <li className={styles.navitem}> <a href="/">About</a></li>
                     <li className={styles.navitem}> <a href="/">Login</a></li>
@@ -20,8 +23,8 @@ export default function NavAndFooter({ children }) {
                 </ul>
             </nav>
             {children}
-            <footer>
-                <p>Footer</p>
+            <footer className={styles.footer}>
+                <p>This is a test</p>
             </footer>
         </main>
     </div>
