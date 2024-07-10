abecd8e232a80b254e79927e1f430e24f5637d21
Jerrell Paul <jerrell@uoguelph.ca>
2024-07-10 17:09:46 -0400

    updated gunicorn to waitress to address compatibility with windows work environment

--- flask/application.py
+++ flask/application.py
@@ -1,5 +1,9 @@
 import os
+from waitress import serve
 from src import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    if os.getenv('FLASK_ENV') == 'development':
+        app.run(debug=True)
+    else:
+        serve(app, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
