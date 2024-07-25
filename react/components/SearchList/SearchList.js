062ca18a36d6104c18c53af7010396c9a0d38758
Jerrell Paul <jerrell@uoguelph.ca>
2024-07-25 17:14:32 -0400

    Added simple pie chart to display data

--- react/components/SearchList/SearchList.js
+++ react/components/SearchList/SearchList.js
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import { PieChart } from 'react-minimal-pie-chart';
+
 import style from './SearchList.module.css';
 
 export default function SearchList(props) {
@@ -14,7 +16,7 @@ export default function SearchList(props) {
                 },
                 body: JSON.stringify({ name: props.input }),
             });
-            const data = await res.json(); // Make sure to await the parsing of the JSON
+            const data = await res.json();
             const items = data?.foods?.food?.map((item) => ({
                 name: item.food_name,
                 description: item.food_description,
@@ -48,10 +50,28 @@ function SearchItem(props) {
         setSelected(!selected);
     }
 
+    const data = props.description.match(/: \d+(\.\d+)?g/g);
+    const pieValues = data.map((item) => parseFloat(item.slice(2, -1)));
+
+
     return (
         <div className={style.searchItem} onClick={handleClick}>
             <h3>{props.name}</h3>
-            {selected ? <p className={style.description}>{props.description}</p> : null}
+            {selected ? <p className={style.description}>{props.description}</p> : null }
+            {
+                selected ? 
+                <div className={style.chart}>
+                    <PieChart
+                        data={[
+                            { title: 'Protein', value: pieValues[2], color: '#E38627' },
+                            { title: 'Carbs', value: pieValues[1], color: '#C13C37' },
+                            { title: 'Fat', value: pieValues[0], color: '#6A2135' },
+                            
+                        ]}
+                    />
+                </div>
+                 : null
+            }
             
         </div>
     )
