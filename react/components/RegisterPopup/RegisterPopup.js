f33680eb3ee77b80352e24386f4ab5559266e8ab
Jerrell Paul <jerrell@uoguelph.ca>
2024-08-17 21:27:58 -0400

    designed form bg

--- react/components/RegisterPopup/RegisterPopup.js
+++ react/components/RegisterPopup/RegisterPopup.js
@@ -34,13 +34,15 @@ export default function RegisterPopup() {
 
     return (
         <div className={styles.container}>
-            <form className={styles.form}>
-                <h1>Register</h1>
-                <FormItem id="email" label="Email" type="text"/>
-                <FormItem id="password" label="Password" type="password"/>
-                <FormItem id="confirmPassword" label="Confirm Password" type="password"/>
-                <button type="submit" onClick={handleSubmit}>Register</button>
-            </form>
+            <div className={styles.overlay}>
+                <form className={styles.form}>
+                    <h1>Register</h1>
+                    <FormItem id="email" label="Email" type="text"/>
+                    <FormItem id="password" label="Password" type="password"/>
+                    <FormItem id="confirmPassword" label="Confirm Password" type="password"/>
+                    <button type="submit" onClick={handleSubmit}>Register</button>
+                </form>
+            </div>
         </div>
     );
 }
