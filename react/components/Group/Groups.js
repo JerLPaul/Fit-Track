9b6d27b715a672005d42940ca0229dbc330db68e
JerLPaul <jerrellpaul10@gmail.com>
2025-06-13 21:42:01 -0400

    component for retrieving and viewing the data from db

---
+++ react/components/Group/Groups.js
@@ -0,0 +1,9 @@
+
+
+export default function Groups() {
+    /*
+        This will take data from supabase and display it in a list format.
+        will be signaled to update if the data changes in supabase.
+    */
+    
+}
